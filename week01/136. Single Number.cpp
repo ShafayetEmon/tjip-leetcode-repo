ebc66de564bcd89de9c25d59a5d62eb2af477c09
Md Shafayet Jamil <45825058+ShafayetEmon@users.noreply.github.com>
2022-04-07 23:59:36 +0600

    136. Single Number

---
+++ week01/136. Single Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        int sz = nums.size();
+        
+        for(int i = 0; i<sz; i++)
+            ans ^=nums[i];
+        
+        return ans;
+        
+    }
+};
+
+//Time Complexity: O(N) Memory Complexity: O(1)
